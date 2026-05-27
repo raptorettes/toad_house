b024ade698ddf48e410f09d1d49549d66beba1ff
raptorettes <gutterpercher@gmail.com>
2026-05-27 22:09:21 +0200

    basic setup

---
+++ scripts/frog.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 100
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+@onready var anim_player = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+var input_direction: Vector2 = Vector2.ZERO
+var current_state: String = ""
+
+
+func _physics_process(_delta):
+	input_direction = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
+	
+	if input_direction.x < 0:
+		sprite.flip_h = true
+	elif input_direction.x > 0:
+		sprite.flip_h = false
+	
+	velocity = input_direction * move_speed
+	move_and_slide()
+	pick_new_state()
+
+func flip_sprite():
+	if abs(input_direction.x) > 0.7:
+		scale.x = -1 if input_direction.x < 0 else 1
+
+func pick_new_state():
+	var new_state = "run" if input_direction != Vector2.ZERO else "idle1"
+	if new_state != current_state:
+		current_state = new_state
+		state_machine.travel(new_state)
