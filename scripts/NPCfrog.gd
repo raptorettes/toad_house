f126943b2d019547761bd3a4c18299cea9ed2e93
raptorettes <gutterpercher@gmail.com>
2026-05-28 17:04:29 +0200

    tilemaps working, frogs randomizing

--- scripts/NPCfrog.gd
+++ scripts/NPCfrog.gd
@@ -9,8 +9,12 @@ extends CharacterBody2D
 
 var home_position: Vector2
 
+@export var skins: Array[Texture2D] = []
+
 func _ready():
 	home_position = global_position
-
+	if skins.size() > 0:
+		sprite.texture = skins[randi() % skins.size()]
+		
 func is_good_position(pos: Vector2) -> bool:
 	return pos.distance_to(home_position) <= wander_radius
