584199b64acb1c471d3fce9055a4b56663e2c7ab
raptorettes <gutterpercher@gmail.com>
2026-05-31 20:06:05 +0200

    nav region + agent + targets

--- scripts/NPCfrog.gd
+++ scripts/NPCfrog.gd
@@ -8,6 +8,7 @@ class_name npc_frogge
 @onready var animation_tree = $AnimationTree
 @onready var state_machine : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
 @onready var sprite = $Sprite2D
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
 var flee_body: CharacterBody2D = null
 var target: Vector2
@@ -22,15 +23,14 @@ func _ready():
 	
 	#$StateChart/Root/Wander.delay_in_seconds = randi_range(5, 10)
 		
+
+
 func _physics_process(_delta):
-	if velocity != Vector2.ZERO:
-		
-		if velocity.x < 0:
-			sprite.flip_h = true
-		elif velocity.x > 0:
-			sprite.flip_h = false
-		
-		move_and_slide()
+	#if nav_agent.has_ta
+	#
+	var next = nav_agent.get_next_path_position()
+	velocity = (next - global_position).normalized() * move_speed
+	move_and_slide()
 		
 func is_good_position(pos: Vector2) -> bool:
 	return pos.distance_to(home_position) <= wander_radius
@@ -39,7 +39,8 @@ func is_good_position(pos: Vector2) -> bool:
 func _on_run_state_entered() -> void:
 	
 	var flee_dir: Vector2 = (global_position - flee_body.global_position)
-	target = global_position + flee_dir * 20
+	#target = global_position + flee_dir * 20
+	target = _get_navigable(global_position + flee_dir * 20)
 
 	_go_to(target)
 	
@@ -50,7 +51,6 @@ func _on_surprise_area_body_entered(body: Node2D) -> void:
 
 
 func _on_surpise_state_entered() -> void:
-	#$Debugger.visible = true
 	state_machine.travel("surprise")
 
 
@@ -61,41 +61,33 @@ func _on_run_area_body_entered(body: Node2D) -> void:
 
 
 func _on_idle_state_entered() -> void:
-	#get_tree().create_timer(2).timeout.connect(func(): $Debugger.visible = false)
 	state_machine.travel("idle%d" % randi_range(1,3))
 	velocity = Vector2.ZERO
 	flee_body = null
 
 
 func _on_wander_state_entered() -> void:
-	#$Debugger.visible = true
-	target = Vector2(global_position.x + randf_range(-50, 50), global_position.y + randf_range(-50, 50))
-
+	#target = Vector2(global_position.x + randf_range(-50, 50), global_position.y + randf_range(-50, 50))
+	target = _get_navigable(Vector2(global_position.x + randf_range(-50, 50), global_position.y + randf_range(-50, 50)))
 	_go_to(target)
 
-func _go_to(target: Vector2) -> void:
-	var raw_direction: Vector2 = (target - global_position).normalized()
-	queue_redraw()
-		
+func _get_navigable(pos: Vector2) -> Vector2:
+	return NavigationServer2D.map_get_closest_point(get_world_2d().navigation_map, pos)
+
+func _go_to(pos: Vector2) -> void:
+	nav_agent.target_position = pos
 	state_machine.travel("run")
 	
-	#var direction: Vector2
-	#if abs(raw_direction.x) > abs(raw_direction.y):
-		#direction = Vector2(sign(raw_direction.x), 0)
-	#else:
-		#direction = Vector2(0, sign(raw_direction.y))
-
-	velocity = raw_direction * move_speed
-
-	if global_position.distance_to(target) < 5:
-		velocity = Vector2.ZERO
-
-func _draw() -> void:
-	draw_line(global_position, (target), Color.GREEN, 1.0)
-	draw_circle(to_local(target), 3, Color.RED)
-
+	if velocity.x < 0:
+		sprite.flip_h = true
+	elif velocity.x > 0:
+		sprite.flip_h = false
 
 func _on_surprise_area_body_exited(body: Node2D) -> void:
 	if body is player_frogge:
 		flee_body = null
 		$StateChart.send_event("character_left")
+
+
+func _on_navigation_agent_2d_navigation_finished() -> void:
+	$StateChart.send_event("nav_finished")
